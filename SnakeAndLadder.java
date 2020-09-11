bebc96cb72ce74232d16e5699afd4aabf37f8039
chsprithvi <chsprithvi@gmail.com>
2020-09-11 17:17:54 +0530

    [Siddhartha]Added:Dice value to code

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,10 +3,20 @@ import java.util.Random;
 public class SnakeAndLadder
 {
     int position=0;
+
+    //Function for rolling a dice
+	public int rollDice()
+	{
+		Random rand=new Random();
+		//Random Number from 1 to 6  
+		int diceResult=rand.nextInt(6)+1;
+		return diceResult;
+	}
     
     public static void main(String[] args) {
     	//obj for player1
         SnakeAndLadder player1 = new SnakeAndLadder();
         System.out.println("Player position:"+player1.position);
+        System.out.println("Dice value:"+player1.rollDice());
 }
 }
