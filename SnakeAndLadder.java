8c9fe268f6e2c7cf6f8da208b49f02fcee2fa178
chsprithvi <chsprithvi@gmail.com>
2020-09-11 17:14:17 +0530

    [Siddhartha]Added:Created player position at 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class SnakeAndLadder
+{
+    int position=0;
+    
+    public static void main(String[] args) {
+    	//obj for player1
+        SnakeAndLadder player1 = new SnakeAndLadder();
+        System.out.println("Player position:"+player1.position);
+}
+}
